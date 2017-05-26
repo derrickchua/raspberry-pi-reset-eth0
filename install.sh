3df3ceea3adab02815d2bc2132f1c0526d7015c9
dced <derrick.2a4@gmail.com>
2017-05-26 16:53:41 +0800

    Corrected install.sh to work in any directory and fixed permissions

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp "$DIR/pi_reseteth0.sh" /home/pi/pi_reseteth0.sh
 chmod +x /home/pi/pi_reseteth0.sh
 
-(sudo crontab -l 2>/dev/null; echo "* * * * * /home/pi/pi_reseteth0.sh") | sudo crontab 
--
+(sudo crontab -l 2>/dev/null; echo "* * * * * /home/pi/pi_reseteth0.sh") | sudo crontab -
